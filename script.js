1deeca814e57fc9a6db8e4c70dffe45589145430
Adama <adamadeze.konate@ynov.com>
2026-03-06 14:53:38 +0100

    feat: integrate toxic floor into main game loop

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 // 1. Configuration du terrain de jeu
 const canvas = document.getElementById('game');
+window.canvas = canvas; // Permet à enemies.js d'accéder au canvas si besoin
 const ctx = canvas.getContext('2d');
 
 // 2. Variables du jeu
 let gameState = 'START'; 
 let score = 0;
+
+// Instanciation du gestionnaire de sol (Le Gardien)
+const floorManager = new FloorManager();
 
 // 3. Objet Joueur (Position, taille, vitesse)
 let player = {
@@ -20,8 +24,16 @@ let keys = {};
 window.addEventListener('keydown', (e) => {
     keys[e.code] = true;
     // Appuyer sur ESPACE pour démarrer le jeu
-    if (e.code === 'Space' && gameState === 'START') {
-        gameState = 'PLAYING';
+    if (e.code === 'Space') {
+        if (gameState === 'START') {
+            gameState = 'PLAYING';
+        } else if (gameState === 'GAMEOVER') {
+            // Réinitialisation du jeu
+            player.x = canvas.width / 2 - 20;
+            player.y = canvas.height / 2 - 20;
+            score = 0;
+            gameState = 'PLAYING';
+        }
     }
 });
 window.addEventListener('keyup', (e) => keys[e.code] = false);
@@ -36,6 +48,12 @@ function update() {
         
         // Le score augmente avec le temps
         score++;
+
+        // Gestion du sol toxique
+        floorManager.update();
+        if (floorManager.checkDanger(player)) {
+            gameState = 'GAMEOVER';
+        }
     }
 }
 
@@ -54,6 +72,9 @@ function draw() {
         ctx.fillText("Press SPACE to Start", canvas.width / 2, canvas.height / 2 + 50);
     } 
     else if (gameState === 'PLAYING') {
+        // Dessiner le sol
+        floorManager.draw(ctx, canvas);
+
         // Dessiner le score
         ctx.fillStyle = "white";
         ctx.font = "20px Arial";
@@ -63,6 +84,15 @@ function draw() {
         // Dessiner le joueur
         ctx.fillStyle = player.color;
         ctx.fillRect(player.x, player.y, player.size, player.size);
+    }
+    else if (gameState === 'GAMEOVER') {
+        ctx.fillStyle = "red";
+        ctx.font = "40px Arial";
+        ctx.textAlign = "center";
+        ctx.fillText("GAME OVER", canvas.width / 2, canvas.height / 2);
+        ctx.fillStyle = "white";
+        ctx.font = "20px Arial";
+        ctx.fillText("Press SPACE to Restart", canvas.width / 2, canvas.height / 2 + 50);
     }
 
     update();
