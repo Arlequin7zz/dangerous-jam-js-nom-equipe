b4e0d924e92dc2b577bbc215e6cd75578ff659ea
Adama <adamadeze.konate@ynov.com>
2026-03-12 14:25:15 +0100

    feat(enemies): implement Enemy class with randomized movement

--- enemies.js
+++ enemies.js
@@ -1,151 +1,31 @@
-class FloorManager {
-
-    constructor() {
-
-        this.floorIsToxic = false;
-
-        this.floorColor = 'green';
-
-       
-
-        this.currentState = 'SAFE';
-
-
-
-        this.timer = 0;
-
-        this.lastTime = Date.now();
-
-
-
-        this.durations = {
-
-            SAFE: 3000,
-
-            WARNING: 1000,
-
-            TOXIC: 2000
-
-        };
-
-
-
-        this.floorHeight = 50;
-
+class Enemy {
+    constructor(canvasWidth, canvasHeight) {
+        this.canvasWidth = canvasWidth;
+        this.canvasHeight = canvasHeight;
+        
+        this.width = 40;
+        this.height = 40;
+        
+        this.x = this.canvasWidth;
+        this.y = Math.random() * (this.canvasHeight - 100 - this.height) + 50;
+        this.speedX = Math.random() * 4 + 3;
+        
+        this.markedForDeletion = false;
+        this.color = 'red';
     }
-
-
 
     update() {
+        this.x -= this.speedX;
 
-        const currentTime = Date.now();
-
-        const deltaTime = currentTime - this.lastTime;
-
-        this.lastTime = currentTime;
-
-
-
-        this.timer += deltaTime;
-
-
-
-        if (this.timer >= this.durations[this.currentState]) {
-
-            this.transitionState();
-
+        if (this.x + this.width < 0) {
+            this.markedForDeletion = true;
         }
-
     }
 
-
-
-    transitionState() {
-
-        this.timer = 0;
-
-
-
-        switch (this.currentState) {
-
-            case 'SAFE':
-
-                this.currentState = 'WARNING';
-
-                this.floorColor = 'orange';
-
-                this.floorIsToxic = false;
-
-                break;
-
-            case 'WARNING':
-
-                this.currentState = 'TOXIC';
-
-                this.floorColor = 'red';
-
-                this.floorIsToxic = true;
-
-                break;
-
-            case 'TOXIC':
-
-                this.currentState = 'SAFE';
-
-                this.floorColor = 'green';
-
-                this.floorIsToxic = false;
-
-                break;
-
-        }
-
+    draw(ctx) {
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.strokeStyle = 'white';
+        ctx.strokeRect(this.x, this.y, this.width, this.height);
     }
-
-
-
-    draw(ctx, canvas) {
-
-        ctx.fillStyle = this.floorColor;
-
-        ctx.fillRect(0, canvas.height - this.floorHeight, canvas.width, this.floorHeight);
-
-    }
-
-
-
-    checkDanger(player) {
-
-        if (!this.floorIsToxic) return false;
-
-
-
-        if (!player) return false;
-
-
-
-        const playerHeight = player.height || player.size || 50;
-
-        const playerBottom = player.y + playerHeight;
-
-
-
-        const canvasHeight = (window.canvas) ? window.canvas.height : 600;
-
-        const floorTop = canvasHeight - this.floorHeight;
-
-
-
-        if (playerBottom >= floorTop) {
-
-            return true;
-
-        }
-
-
-
-        return false;
-
-    }
-
 }
